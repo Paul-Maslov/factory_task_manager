0e6c7bde6b992a7be04ae73d009122f204f95a47
Paul Maslov <maslovp67@gmail.com>
2023-04-02 02:53:01 +0300

    Create 1 part of DB vodels(Position,Employee,Post,Comment. Customize adminpanel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from task_manager.models import Post, Commentary, Employee, Position
+
+admin.site.unregister(Group)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["owner", "title", "content", "created_time"]
+    list_filter = ["created_time", "owner"]
+    search_fields = ["title"]
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ["user", "post", "content", "created_time"]
+    list_filter = ["post", "created_time"]
+    search_fields = ["post"]
+
+
+@admin.register(Employee)
+class EmployeeAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("position",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name", "position",)}),
+    )
--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,63 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=75, null=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="employees"
+    )
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "employee"
+        verbose_name_plural = "employees"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(null=True, blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
+    )
+
+    class Meta:
+        ordering = ["created_time"]
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        name="user",
+        related_name="commentaries",
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        name="post",
+        related_name="commentaries",
+    )
+    content = models.TextField(null=True, blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_time"]
