ad5dbeaa21e00cdc4ea1123900288d1727373fac
Paul Maslov <maslovp67@gmail.com>
2023-04-19 11:54:44 +0300

    Added mediafiles to project deploy v.03

--- factory_task_manager/settings.py
+++ factory_task_manager/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "mediafiles")
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "mediafiles")
 # STATIC_URL = "/static/"
