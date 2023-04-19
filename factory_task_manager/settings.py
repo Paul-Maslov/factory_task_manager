8cf4e8d230d64375f6e235e2921f2f29982eae1c
Paul Maslov <maslovp67@gmail.com>
2023-04-19 11:44:38 +0300

    Added mediafiles to project deploy

--- factory_task_manager/settings.py
+++ factory_task_manager/settings.py
@@ -136,13 +136,12 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "mediafiles")
 # STATIC_URL = "/static/"
 # STATICFILES_DIRS = (BASE_DIR / "static",)
 #
 # STATIC_ROOT = "static"
-MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
