432b0277ec99cb7ff995504ffe6eaa9e5811eea0
Paul Maslov <maslovp67@gmail.com>
2023-04-19 12:37:08 +0300

    Added mediafiles v.05

--- factory_task_manager/settings.py
+++ factory_task_manager/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "mediafiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "mediafiles")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 # STATIC_URL = "/static/"
 # STATICFILES_DIRS = (BASE_DIR / "static",)
 #
